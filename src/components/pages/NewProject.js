5d2956fb81ec39816bfe7a3289a4c4d7578a6884
hufeli <hudsfern@gmail.com>
2022-07-02 11:54:30 -0300

    update

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,6 +1,15 @@
-function NewProject() {
+import ProjectForm from '../project/ProjectForm'
 
-    return <h1>New Project</h1>
+import styles from './NewProject.module.css'
+
+function NewProject() {
+    return (
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar o serviços</p>
+            <ProjectForm btnText="Criar Projeto"/>
+        </div>
+    )
 }
 
 export default NewProject
---
+++ src/components/project/ProjectForm.js
@@ -0,0 +1,35 @@
+import Input from '../form/Input'
+import Select from '../form/Select'
+import SubmitButton from '../form/SubmitButton'
+
+import styles from './ProjectForm.module.css' 
+
+function ProjectForm({btnText}) {
+  return (
+    <form className={styles.form}>
+      <div>
+        <Input
+          type="text"
+          text="Nome do projeto"
+          name="name"
+          placeholder="Insira o nome do projeto"
+        />
+      </div>
+      <div>
+        <Input
+          type="number"
+          text="Orçamento do projeto"
+          name="budget"
+          placeholder="Insira o orçamento total"
+        />
+      </div>
+      <div>
+        <Select name="category_id" text="Selecione a categoria" />
+      </div>
+      <SubmitButton text={btnText}/>
+      
+    </form>
+  );
+}
+
+export default ProjectForm;
