87e4a2feb41c0520be168d22a1f6aba58084a825
hufeli <hudsfern@gmail.com>
2022-07-12 09:49:08 -0300

    update

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,18 +1,48 @@
+import { useEffect, useState } from 'react'
+
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 
-import styles from './ProjectForm.module.css' 
+import styles from './ProjectForm.module.css'
 
-function ProjectForm({btnText}) {
+function ProjectForm({ handleSubmit, btnText, ProjectData }) {
+
+  const [categories, setCategories] = useState([])
+  const [project, setProject] = useState(ProjectData || {})
+
+  useEffect(() => {
+    fetch('http://localhost:5000/categories', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCategories(data)
+      }).catch((err) => console.log(err))
+  }, [])
+
+  const submit = (e) => {
+    e.preventDefault()
+    handleSubmit(project)
+  }
+
+  function handleChange(e) {
+    setProject({ ...project, [e.target.name]: e.target.value })
+    console.log(project)
+  }
+
   return (
-    <form className={styles.form}>
+    <form onSubmit={submit} className={styles.form}>
       <div>
         <Input
           type="text"
           text="Nome do projeto"
           name="name"
           placeholder="Insira o nome do projeto"
+          handleOnChange={handleChange}
         />
       </div>
       <div>
@@ -21,15 +51,16 @@ function ProjectForm({btnText}) {
           text="Orçamento do projeto"
           name="budget"
           placeholder="Insira o orçamento total"
+          handleOnChange={handleChange}
         />
       </div>
       <div>
-        <Select name="category_id" text="Selecione a categoria" />
+        <Select name="category_id" text="Selecione a categoria" options={categories} />
       </div>
-      <SubmitButton text={btnText}/>
-      
+      <SubmitButton text={btnText} />
+
     </form>
-  );
+  )
 }
 
 export default ProjectForm;
